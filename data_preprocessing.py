8927714a67dbc34e2c9acccaa7f64322e1e57ae6
Qinghe Li <derrors@foxmail.com>
2020-02-02 18:19:56 +0800

    update the baseline model

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -9,16 +9,21 @@ from transformers import BertTokenizer
 
 
 def get_start_and_end(context, start, text):
+    '''
+    Get the start and end positions of the answer in the review.
+    '''
     if start - 1 < 0:
         start = 0
     else:
         start = start - 1
 
+    # find the start position in the review and compute the end position
     start = context.find(text, start)
     end = start + len(text) - 1
 
     char2id = dict()
     i = 0
+    # convert the char positions to word index in the review sentences
     for idx, c in enumerate(context):
         if c == ' ':
             i += 1
@@ -29,12 +34,17 @@ def get_start_and_end(context, start, text):
 
 
 def extract_example(file_path, tokenizer, max_seq_length):
+    '''
+    Extract examples from the original dataset.
+    Convert the dataset to the format of model inputs.
+    '''
     all_input_ids = []
     all_masks = []
     all_segment_ids = []
     all_answer_starts = []
     all_answer_ends = []
 
+    # load the dataset
     with open(file_path, 'r', encoding='utf-8') as reader:
         dataset = json.load(reader)['data']
 
@@ -44,37 +54,45 @@ def extract_example(file_path, tokenizer, max_seq_length):
             context = ' '.join(tokens)
 
             for qa in paragraph['qas']:
-                sequence = tokens.copy()
+                sequence = tokenizer.tokenize(qa['question'])
+                question_len = len(sequence)
                 sequence.insert(0, '[CLS]')
                 sequence.append('[SEP]')
 
                 segment_ids = [0] * len(sequence)
-                sequence.extend(tokenizer.tokenize(qa['question']))
+                sequence.extend(tokens)
                 sequence.append('[SEP]')
+                segment_ids.extend([1] * (max_seq_length - len(segment_ids)))
 
                 mask = [1] * len(sequence)
                 mask.extend([0] * (max_seq_length - len(mask)))
-
-                segment_ids.extend([1] * (max_seq_length - len(segment_ids)))
 
                 start = qa['answers'][0]['answer_start']
                 text = qa['answers'][0]['text']
                 text = ' '.join(tokenizer.tokenize(text))
 
                 answer_start, answer_end = get_start_and_end(context, start, text)
+                answer_start = answer_start + question_len + 2
+                answer_end = answer_end + question_len + 2
+
                 input_ids = tokenizer.convert_tokens_to_ids(sequence)
 
                 if len(input_ids) < max_seq_length:
                     input_ids.extend([0] * (max_seq_length - len(input_ids)))
                 else:
                     input_ids = input_ids[: max_seq_length]
+                    mask = mask[: max_seq_length]
+                    segment_ids = segment_ids[: max_seq_length]
 
-                all_input_ids.append(input_ids)
-                all_masks.append(mask)
-                all_segment_ids.append(segment_ids)
-                all_answer_starts.append(answer_start)
-                all_answer_ends.append(answer_end)
+                all_input_ids.append(input_ids)             # input ids
+                all_masks.append(mask)                      # attention masks
+                all_segment_ids.append(segment_ids)         # type of tokens
+                all_answer_starts.append(answer_start)      # start position
+                all_answer_ends.append(answer_end)          # end position
 
+    assert len(all_input_ids) == len(all_masks) == len(all_segment_ids) == len(all_answer_starts) == len(all_answer_ends)
+
+    # convert to tensor
     all_input_ids = torch.tensor(all_input_ids, dtype=torch.long)
     all_masks = torch.tensor(all_masks, dtype=torch.long)
     all_segment_ids = torch.tensor(all_segment_ids, dtype=torch.long)
--- run_rrc.py
+++ run_rrc.py
@@ -11,47 +11,165 @@ import numpy as np
 
 from apex.optimizers import FP16_Optimizer, FusedAdam
 from transformers import BertTokenizer, BertForQuestionAnswering, AdamW
-from torch.utils.data import TensorDataset, DataLoader, RandomSampler
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 from data_preprocessing import extract_example
-from tqdm import trange
 
 
-def train(args, tokenizer, model, optimizer):
-    os.path.join(args.data_dir, 'train.json')
+def to_list(tensor):
+    '''
+    Get the data from the GPUs and convert to lists.
+    '''
+    return tensor.detach().cpu().tolist()
 
-    all_input_ids, all_masks, all_segment_ids, all_answer_starts, all_answer_ends = extract_example(os.path.join(args.data_dir, 'train.json'), tokenizer, args.max_seq_length)
 
-    train_data = TensorDataset(all_input_ids, all_masks, all_segment_ids, all_answer_starts, all_answer_ends)
+def convert_data(starts, ends):
+    '''
+    Convert the answer positions to answer spans list.
+    A list of numbers represent a list of words of the answers.
+    '''
+    assert len(starts) == len(ends)
+
+    spans = []
+    for start, end in zip(starts, ends):
+        span = np.arange(start, end + 1)
+        spans.append(span)
+    return spans
+
+
+def train(args, dataloader, model, optimizer):
+    '''
+    Training the BERT model on RRC dataset.
+    '''
+
+    loss_avg = 0                # the average loss during a complete epoch of training
+    model.train()
+    for batch_data in dataloader:
+        batch_data = tuple(d.cuda() for d in batch_data)
+        input_ids, masks, segment_ids, answer_starts, answer_ends = batch_data
+
+        loss, _, _ = model(input_ids=input_ids,
+                           attention_mask=masks,
+                           token_type_ids=segment_ids,
+                           start_positions=answer_starts,
+                           end_positions=answer_ends)
+
+        if args.fp16:
+            optimizer.backward(loss)
+        else:
+            loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        loss_avg += loss.item()
+    loss_avg = loss_avg / len(dataloader)
+
+    return loss_avg
+
+
+def test(args, dataloader, model):
+    '''
+    Testing the BERT model on RRC dataset.
+    '''
+    ground_truth = []
+    prediction = []
+
+    model.eval()
+    for batch_data in dataloader:
+        batch_data = tuple(d.cuda() for d in batch_data)
+        input_ids, masks, segment_ids, answer_starts, answer_ends = batch_data
+
+        with torch.no_grad():
+            start_scores, end_scores = model(input_ids=input_ids,
+                                             attention_mask=masks,
+                                             token_type_ids=segment_ids)
+
+        # convert start and end scores to index
+        pred_starts = np.argmax(to_list(start_scores), axis=1)
+        pred_ends = np.argmax(to_list(end_scores), axis=1)
+        pred_spans = convert_data(pred_starts, pred_ends)
+        prediction.extend(pred_spans)
+        true_spans = convert_data(to_list(answer_starts), to_list(answer_ends))
+        ground_truth.extend(true_spans)
+
+    assert len(prediction) == len(ground_truth)
+    return prediction, ground_truth
+
+
+def evaluate(prediction, ground_truth):
+    '''
+    Evaluate the trained model on the testing dataset.
+    Return the EM(Exact Match) and F1 scores.
+    '''
+    def exact_match(pred, true):
+        if list(pred) == list(true):
+            return 1
+        else:
+            return 0
+
+    def f1_score(pred, true):
+        if list(pred) == list(true):
+            return 1.0
+        else:
+            num_same = 0
+            for n in pred:
+                if n in true:
+                    num_same += 1
+
+            if len(pred) == 0:
+                precision = 0.0
+            else:
+                precision = 1.0 * num_same / len(pred)
+            recall = 1.0 * num_same / len(true)
+            # avoid dividing zero
+            if (precision + recall) == 0:
+                f1 = 0
+            else:
+                f1 = (2 * precision * recall) / (precision + recall)
+            return f1
+
+    num_exact_match = 0
+    f1 = 0
+    for pred, true in zip(prediction, ground_truth):
+        num_exact_match += exact_match(pred, true)
+        f1 += f1_score(pred, true)
+
+    metrics = {
+        'EM': num_exact_match / len(prediction),
+        'F1': f1 / len(prediction)
+    }
+    return metrics
+
+
+def train_and_test(args, tokenizer, model, optimizer):
+    '''
+    Model training and testing on RRC dataset.
+    '''
+    # load the preprocessed data
+    train_input_ids, train_masks, train_segment_ids, train_answer_starts, train_answer_ends = extract_example(os.path.join(args.data_dir, 'train.json'), tokenizer, args.max_seq_length)
+
+    # build the data loader of training and testing datasets
+    train_data = TensorDataset(train_input_ids, train_masks, train_segment_ids, train_answer_starts, train_answer_ends)
     train_sampler = RandomSampler(train_data)
     train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=args.batch_size)
 
-    model.train()
+    test_input_ids, test_masks, test_segment_ids, test_answer_starts, test_answer_ends = extract_example(os.path.join(args.data_dir, 'test.json'), tokenizer, args.max_seq_length)
+    test_data = TensorDataset(test_input_ids, test_masks, test_segment_ids, test_answer_starts, test_answer_ends)
+    test_sampler = SequentialSampler(test_data)
+    test_dataloader = DataLoader(test_data, sampler=test_sampler, batch_size=args.batch_size)
 
-    for t in trange(args.epochs):
-        loss_avg = 0
-        for batch_data in train_dataloader:
-            batch_data = tuple(d.cuda() for d in batch_data)
-            input_ids, masks, segment_ids, answer_starts, answer_ends = batch_data
+    # start trianing and testing: after every epoch, do a evaluation of the model
+    for i in range(args.epochs):
+        loss = train(args, train_dataloader, model, optimizer)
 
-            loss, start_scores, end_scores = model(input_ids=input_ids,
-                                                   attention_mask=masks,
-                                                   token_type_ids=segment_ids,
-                                                   start_positions=answer_starts,
-                                                   end_positions=answer_ends)
+        test_pred, test_ground = test(args, test_dataloader, model)
+        test_metrics = evaluate(test_pred, test_ground)
 
-            if args.fp16:
-                optimizer.backward(loss)
-            else:
-                loss.backward()
+        print('Epoch[{:<2d}/{:<2d}]: loss: {:.4f}  EM: {:.4f}  F1:{:.4f}'.format(
+            i + 1, args.epochs, loss, test_metrics['EM'] * 100, test_metrics['F1'] * 100))
 
-            optimizer.step()
-            optimizer.zero_grad()
 
-            loss_avg += loss
-
-        t.set_postfix(loss='{:05.3f}'.format((loss_avg / len(train_dataloader)).item())
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    # Running arguments
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--bert_model',
@@ -63,43 +181,44 @@ if __name__ == "__main__":
                         type=str,
                         help='The input data dir containing json files.')
 
-    parser.add_argument("--output_dir",
+    parser.add_argument('--output_dir',
                         default='./saved model checkpoints',
                         type=str,
-                        help="The output directory where the model checkpoints will be written.",
+                        help='The output directory where the model checkpoints will be written.',
                         )
 
     parser.add_argument('--batch_size',
                         default=32,
                         type=int,
-                        help="Total batch size for training.")
+                        help='Total batch size for training.')
 
-    parser.add_argument("--epochs",
-                        default=20,
+    parser.add_argument('--epochs',
+                        default=40,
                         type=int,
-                        help="Total number of training epochs to perform.")
+                        help='Total number of training epochs to perform.')
 
-    parser.add_argument("--max_seq_length",
+    parser.add_argument('--max_seq_length',
                         default=512,
                         type=int,
-                        help="The maximum total input sequence length after WordPiece tokenization. \n"
-                             "Sequences longer than this will be truncated, and sequences shorter \n"
-                             "than this will be padded.")
+                        help='The maximum total input sequence length after WordPiece tokenization. \n'
+                             'Sequences longer than this will be truncated, and sequences shorter \n'
+                             'than this will be padded.')
 
-    parser.add_argument("--learning_rate",
+    parser.add_argument('--learning_rate',
                         default=3e-5,
                         type=float,
-                        help="The initial learning rate for Adam.")
+                        help='The initial learning rate for Adam.')
 
     parser.add_argument('--fp16',
                         default=True,
                         action='store_true',
-                        help="Whether to use 16-bit float precision instead of 32-bit")
+                        help='Whether to use 16-bit float precision instead of 32-bit')
 
     args = parser.parse_args()
 
     torch.manual_seed(2020)
 
+    # load tokenizer and BERT model
     tokenizer = BertTokenizer.from_pretrained(args.bert_model)
     model = BertForQuestionAnswering.from_pretrained(args.bert_model)
 
@@ -121,8 +240,7 @@ if __name__ == "__main__":
                               bias_correction=False,
                               max_grad_norm=1.0)
         optimizer = FP16_Optimizer(optimizer, dynamic_loss_scale=True)
-
     else:
         optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate)
 
-    train(args, tokenizer, model, optimizer)
+    train_and_test(args, tokenizer, model, optimizer)
